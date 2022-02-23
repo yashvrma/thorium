dd969e6bd147e6bc4dacf1bb9ba9518c0ead4330
Yash Verma <yashh1908@gmail.com>
2022-02-23 22:59:03 +0530

    abcd

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+router.get('/movies', function(req, res) {
+    res.send('["Fuckre", "Delhi","Dabang","Rockstar",Suryavash"]')
+});
+
+router.get('./movies/:movieId',  function(req, res){
+
+    movi=["Fuckrey", "Spiderman", "Dabang", "rockstar", "Suryavanshi"]
+let value = req.param.movieId;
+if(value>mov.length-1){
+    res.send("Invalid Input")
+}else{
+    res.send(movi[value])
+}
+})
+
+router.get('/films/:filmid', function(req, res){
+    let movies=[{id: 1,name: 'The Shining'},{id: 2, name: 'Incendies'},{id:3, name:'Rang debasanti'},{id:4, name:'Finding demo'}]
+    let value = req.param.filmId;
+    let abc = false
+
+    for(let i=0; i<movies.length; i++){
+        if(movies[i].id==value){
+            abc = true;
+            res.send(movies[i])
+            break;
+        }
+    }
+    if(abc =false){
+        res.send("Invalid Input");
+    }
 })
 
 
 
+// its my assignment
 
 module.exports = router;
